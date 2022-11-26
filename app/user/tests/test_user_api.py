8b205095de731f8d3661b9c5c432b76d532f70e9
jlbfb <38572799+jlbfb@users.noreply.github.com>
2022-11-25 20:51:15 -0700

    Commit to capture all code through lesson 85: Implement create recipe API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -108,6 +108,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
 class PrivateUserApiTests(TestCase):
     """Tests API requests that require authentication."""
 
